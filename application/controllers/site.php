2926f83aabca12b605f6d7c1d10bd4f8a906ee09
Nguyen Hoang Ron <ron.nguyen@audiencemedia.com>
2014-06-25 10:57:09 +0700

    * create webserver for alophuot

---
+++ application/controllers/site.php
@@ -0,0 +1,24 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Site extends CI_Controller {
+	public function index(){
+		$this->home();
+	}
+	
+	public function home(){
+		$data['title'] = "Home!";
+		$data['var1'] = "2";
+		$data['var2'] = "8";
+		
+		$this->load->model("math");
+		
+		$data['addTotal'] = $this->math->add($data['var1'],$data['var2']);
+		$data['subTotal'] = $this->math->sub($data['var1'],$data['var2']);
+		$this->load->view("view_home",$data);
+	}
+	
+	public function about(){
+		$data['title'] = "About!";
+		$this->load->view("view_about");
+	}
+}
