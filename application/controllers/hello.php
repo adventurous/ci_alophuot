e9050ed6a3806cbec353c4dbb70d164fc3e4ac47
Hoang Ron <ron.nguyenhoang@gmail.com>
2014-06-26 07:45:40 +0700

    * update function insert, update with db.

--- application/controllers/hello.php
+++ application/controllers/hello.php
@@ -21,5 +21,20 @@
           $data["results"] = $this->get_db->get_All();
           $this->load->view('view_db',$data);
       }
+      
+      public function insertValues(){
+          $newRows = array(array("name" => "susan"),array("name" => "susan"));
+          $this->load->model('get_db');
+          $this->get_db->insert2($newRows);
+          echo "Inserted successful";
+      }
+      
+      public function updateValue(){
+          $data = array("name" => "Hoang Ron");
+          $this->load->model('get_db');
+          $this->get_db->update1($data);
+          
+          echo "Updated successful";
+      }
   }
 ?>
--- application/models/get_db.php
+++ application/models/get_db.php
@@ -13,4 +13,19 @@ class Get_db extends CI_Model{
         $query = $this->db->query("SELECT * FROM test");
         return $query->result();
     }
+    
+    function insert1($data){
+        $this->load->database();
+        $this->db->insert("test",$data); 
+    }
+    
+    function insert2($data){
+        $this->load->database();
+        $this->db->insert_batch("test",$data); 
+    }
+    
+    function update1($data){
+        $this->load->database();
+        $this->db->update("test",$data,"id = 2"); 
+    }
 }
